f5d9918feb92ac17d937c04e3d25ddcbd07d5db5
Ha Seo Hyun <97388352+HaSeoHyun@users.noreply.github.com>
2025-08-22 20:29:17 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    //최소공약수
+    int gcd_num = gcd(n,m);
+    int lcm_num = lcm(n,m);
+    answer.push_back(gcd_num);
+    answer.push_back(lcm_num);
+    return answer;
+}
